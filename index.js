c98cbe1831b8e6a4d50717ed2e88d0853d341ffb
Qays Ayad <work.worker55@gmail.com>
2020-05-16 15:56:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
   console.log("The DOM has loaded");
   const p = document.getElementById('text');
   p.textContent = "This is really cool!";
-  
+
 });
 
 console.log(
